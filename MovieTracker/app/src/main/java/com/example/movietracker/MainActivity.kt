7f3c8f67b9c72c2256667d1e11c7e242d35ffe74
Antoni <antek.karon@gmail.com>
2024-12-11 23:43:30 +0100

    fixing code, adding documentation

--- MovieTracker/app/src/main/java/com/example/movietracker/MainActivity.kt
+++ MovieTracker/app/src/main/java/com/example/movietracker/MainActivity.kt
@@ -42,8 +42,7 @@ class MainActivity : AppCompatActivity() {
         ratingLabel = findViewById(R.id.ratingLabel)
         saveButton = findViewById(R.id.saveButton)
         deleteButton = findViewById(R.id.deleteButton)
-
-        // Nasłuchiwacze
+        
         findViewById<Button>(R.id.searchButton).setOnClickListener { searchMovie(movieInput.text.toString()) }
         saveButton.setOnClickListener { saveCurrentMovie() }
         deleteButton.setOnClickListener { deleteCurrentMovie() }
